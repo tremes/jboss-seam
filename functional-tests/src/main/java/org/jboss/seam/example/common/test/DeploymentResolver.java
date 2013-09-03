312758d89029c57ea47c377fda549a0301fe536b
Ron Smeral <rsmeral@redhat.com>
2013-09-03 14:40:16 +0200

    Tests: DeploymentResolver now uses deployment from filesystem

--- functional-tests/src/main/java/org/jboss/seam/example/common/test/DeploymentResolver.java
+++ functional-tests/src/main/java/org/jboss/seam/example/common/test/DeploymentResolver.java
@@ -1,38 +1,23 @@
 package org.jboss.seam.example.common.test;
 
+import java.io.File;
 import org.jboss.shrinkwrap.api.Archive;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.importer.ZipImporter;
 import org.jboss.shrinkwrap.api.spec.EnterpriseArchive;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
-import org.jboss.shrinkwrap.resolver.api.maven.Maven;
-import org.jboss.shrinkwrap.resolver.api.maven.MavenResolvedArtifact;
-import org.jboss.shrinkwrap.resolver.api.maven.PackagingType;
-import org.jboss.shrinkwrap.resolver.api.maven.PomEquippedResolveStage;
 
 public class DeploymentResolver {
 
     public static Archive<?> createDeployment() {
+        String examplesHome = System.getProperty("seam.examples.home");
+        String deploymentPath = SeamGrapheneTest.getProperty("DEPLOYMENT_PATH");
+        String deploymentName = deploymentPath.substring(deploymentPath.lastIndexOf("/") + 1);
 
-        // use profiles defined in 'maven.profiles' property in pom.xml
-        String profilesString = System.getProperty("maven.profiles");
-        String[] profiles = profilesString != null ? profilesString.split(", ?") : new String[0];
-        PomEquippedResolveStage mvn = Maven.resolver().loadPomFromFile("pom.xml", profiles);
-
-        // resolves an artifact with coordinates given in property DEPLOYMENT_ARTIFACT in /ftest.properties
-        MavenResolvedArtifact artifact = mvn.resolve(SeamGrapheneTest.getProperty("DEPLOYMENT_ARTIFACT"))
-                .withoutTransitivity().asSingleResolvedArtifact();
-
-        // use correct archive type
-        PackagingType packaging = artifact.getCoordinate().getPackaging();
-        Class<? extends Archive<?>> deploymentClass = EnterpriseArchive.class;
-        if (packaging == PackagingType.WAR) {
-            deploymentClass = WebArchive.class;
-        }
-
-        //  this is ugly due to ARQ-1390; need to get rid of dots in archive name, otherwise it would be
-        //  return ShrinkWrap.createFromZipFile(deploymentClass, artifact.asFile()).as(deploymentClass);
-        return ShrinkWrap.create(ZipImporter.class, artifact.getCoordinate().getArtifactId() + "." + packaging)
-                .importFrom(artifact.asFile()).as(deploymentClass);
+        Class<? extends Archive<?>> deploymentClass = deploymentName.endsWith(".war")?WebArchive.class:EnterpriseArchive.class;
+        
+        return ShrinkWrap.create(ZipImporter.class, deploymentName)
+                .importFrom(new File(examplesHome + "/" + deploymentPath))
+                .as(deploymentClass);
     }
 }
